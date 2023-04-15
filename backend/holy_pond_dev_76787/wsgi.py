9216bef197f512e99a7cc162709e23398e631bed
Crowdbotics <engineering@crowdbotics.com>
2023-04-15 01:23:33 +0000

    Initial commit.

---
+++ backend/holy_pond_dev_76787/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for holy_pond_dev_76787 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'holy_pond_dev_76787.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-pond-dev-76787",
+  url: "https://holy-pond-dev-76787.botics.co",
+  api: "https://holy-pond-dev-76787.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
